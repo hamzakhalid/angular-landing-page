18e32ea91d85a9fa305ccc5bc34dc8075ddf3b00
Hamza Khalid <pucit.hamza@gmail.com>
2024-12-10 20:25:51 +0500

    Added actionlist component

---
+++ src/app/actionlist/actionlist.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-actionlist',
+  imports: [],
+  templateUrl: './actionlist.component.html',
+  styleUrl: './actionlist.component.scss'
+})
+export class ActionlistComponent {
+
+}
